71532ad666b41e146071993ec0a3413bf8ef6ef2
Tarundeshmukh <tarundeshmukh1997@gmail.com>
2022-10-05 15:49:28 +0530

    added-user-service

---
+++ src/app/user.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  createUser(user:{name:string,age:number;gender:string})
+  constructor() { }
+}
