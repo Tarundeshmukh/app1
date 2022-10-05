48b60f7b5c5e887752f49b6671ed8332d9c8f92c
Tarundeshmukh <tarundeshmukh1997@gmail.com>
2022-10-05 15:49:58 +0530

    save-function-modified

--- src/app/userform/userform.component.ts
+++ src/app/userform/userform.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-userform',
@@ -7,17 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserformComponent implements OnInit {
 
-  constructor() { }
+  constructor(public userService:UserService) { }
 
   ngOnInit(): void {
   }
 
   user = {
     name:"Tarun",
-    age:24
+    age:24,
+    gender:"Male"
   }
 
   save(){
+    this.userService.createUser(this.user)
     console.log("Hello user!");
     console.log(this.user.name);
     
